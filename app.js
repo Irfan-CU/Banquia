c92bff655ad135264ba7c56de2fc202380cb17e1
IrfanMustafa <irfanmustafa31201@gmail.com>
2020-01-04 00:34:24 -0500

    Module Setting

---
+++ app.js
@@ -0,0 +1,24 @@
+var budgetcontroller = (function() {
+
+
+
+})();
+
+/* UI Module*/
+
+var UIController = (function() {
+    //UI Code 
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    document.querySelector('.add__btn').addEventListener('click', function() {
+
+        // get the  input data
+        // Add the new item
+        //Expense and the Inocme calculation
+        // Diplau on UI
+
+    });
+
+})(budgetcontroller, UIController);
